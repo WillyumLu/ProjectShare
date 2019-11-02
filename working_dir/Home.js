df9ed63136e28cd2dfe0653d62cd55ffc51b32c7
GKHalbert <53063801+GKHalbert@users.noreply.github.com>
2019-11-01 22:58:00 -0400

    Login button on nevigation bar works now

---
+++ working_dir/Home.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import Project from './Project';
+import ProjectView from './ProjectView'
+import { uid } from 'react-uid';
+import './Home.css';
+
+var Center = require('react-center');
+
+const global_project_0 = {
+	id: 0,
+	title: "Project1",
+	start_date: "YYYY-MM-DD",
+	status: "in progress",
+	icon: 'url/..../'
+}
+const global_project_1 = {
+	id: 1,
+	title: "Project2",
+	start_date: "000000-MM-DD",
+	status: "in progress",
+	icon: 'url/..../'
+}
+const global_project_2 = {
+	id: 2,
+	title: "Project3",
+	start_date: "000000-MM-DD",
+	status: "complete",
+	icon: 'url/..../'
+}
+
+function loadData() {
+	console.log("loadData() called")
+	/*const projects = [global_project_0, global_project_1, global_project_2]*/
+	const projects = {"Project1": global_project_0, "Project2": global_project_1, "Project3": global_project_2}
+	return projects
+}
+
+
+class Home extends React.Component {
+
+	constructor(props) { // When the componenet is created,  calls load data
+		super(props)
+	  	this.state = {projects: loadData(),
+	  				  displaySelectedProject: false,
+	  				  selectedProject: ''}
+	  }
+
+	receiveSelectedProject = (projectData) => {
+      this.setState({selectedProject: projectData})
+      this.setState({displaySelectedProject: true})
+      console.log(this.state.displaySelectedProject)
+	}
+
+	render() {
+		 if(this.state.displaySelectedProject){
+		 	return(
+			 	<div>
+			 		<ProjectView project={this.state.projects[this.state.selectedProject]}/>
+			 	</div>
+		 	)
+		 }	
+		 else{
+			 return(
+			 	<div id="MainView">
+			 		<h1 className = "Header"> Project Share </h1>
+			 		<Center>
+			 		<div className = "ProjectContainer">
+					{
+						Object.keys(this.state.projects).map((project) => {
+							return(
+								<Project key={uid(project)}
+								         project = {this.state.projects[project]}
+								         sendSelectedProject = {this.receiveSelectedProject}
+								/>
+								)
+
+						})
+					}
+					</div>
+					</Center>					
+				</div>
+			)
+		}
+	}
+}
+
+export default Home;
---
+++ working_dir/nevigation.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Menu, /**Icon, **/ Input } from 'antd';
+import { Link } from 'react-router-dom';
+import 'antd/dist/antd.css';
+
+const { Search } = Input;
+
+class Nevigation extends React.Component {
+  state = {
+    current: 'mail',
+  };
+
+  handleClick = e => {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+  };
+
+  render() {
+    return (
+      <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal"  style={{'marginTop': 10}}>
+        <Menu.Item key="logo">
+        	LOGO
+        </Menu.Item>
+        <Menu.Item key="LOGIN" id="floatRight">
+          <Link to={'/login'}>
+            LOGIN      
+					</Link>
+        </Menu.Item>
+        <Menu.Item key="search" >
+				<Search
+			      placeholder="project you wanna search"
+			      enterButton="Search"
+			      style={{'verticalAlign': 'middle'}}
+			      onSearch={value => console.log(value)}
+			    />
+
+        </Menu.Item>
+        <Menu.Item key="SIGNUP" id="floatRight">
+        SIGNUP
+        </Menu.Item>
+      </Menu>
+    );
+  }
+}
+export default Nevigation;
