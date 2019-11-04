20b0a5f824fec742994b874107efdcd203134b42
GKHalbert <53063801+GKHalbert@users.noreply.github.com>
2019-11-03 20:59:15 -0500

    compelete admin view;

--- working_dir/src/AdminView.js
+++ working_dir/src/AdminView.js
@@ -4,7 +4,9 @@ import QueueAnim from 'rc-queue-anim';
 
 
 import { Redirect } from 'react-router-dom'
-import { message, Avatar, Layout, List, Card, Descriptions, Collapse, Form, Input,Button, Modal,Tag, Icon, Upload} from 'antd';
+import { message, Avatar, Layout, List, Card, Descriptions, Collapse, Form, Input,Button, Modal,Tag, Icon, Upload, Divider, Table, Popconfirm, Select} from 'antd';
+
+const { Option } = Select;
 const { Content } = Layout;
 const { Meta } = Card;
 const { Panel } = Collapse;
@@ -13,18 +15,80 @@ const log = console.log
 const userdata = {
     userName: "admin",
     pwd: "admin",
-    profileImage: require('./userprofile.jpg'),
-    firstName: "Jason",
-    lastName: "Bourne",
-    school: "University of Toronto",
-    bio: "Jesus christ it's jason bourne!",
-    email: "",
-    phone: "114514",
-    projects: [{id: 0, title: "Project1", avatar: null, status: "in progress", link: "/"}, 
+    profileImage: require('./admin.jpg'),
+    firstName: "Donald",
+    lastName: "Trump",
+    orgnazation: "University of Toronto",
+    bio: '"I Am Inevitable."',
+    email: "admin@mail.com",
+    phone: "1919810",
+    projects: [{id: 0, title: "Project1", avatar: null, status: "in progress", link: '/'}, 
                 {id: 1, title: "Project2", avatar: null, status: "in progress"},
-                {id: 2, title: "Project3", avatar: null, status: "complete"}]
+                {id: 2, title: "Project3", avatar: null, status: "complete"},
+                {id: 3, title: "Project4", avatar: null, status: "in progress"},
+                {id: 4, title: "Project5", avatar: null, status: "in progress"}]
 }
 
+const websiteData = {
+    users: [{userName: "user", userId:0, pwd: "user", avatar:require('./userprofile.jpg'),type: "user"},
+    {userName: "Alpha", userId:1, pwd: "123123", avatar:null, type: "user"},
+    {userName: "Bravo", userId:2, pwd: "barvo", avatar:null, type: "user"},
+    {userName: "Charlie", userId:3, pwd: "sadasdasd",avatar:null, type: "user"},
+    {userName: "Delta", userId:4, pwd: "asdasdads", avatar:null, type: "user"},
+    {userName: "Foxtrot", userId:5, pwd: "wwwwweww", avatar:null, type: "user"},
+    {userName: "Golf", userId:6, pwd: "lol",avatar:null, type: "user"},
+    {userName: "Hotel", userId:7, pwd: "lmao",avatar:null,type: "user"},
+    {userName: "India", userId:8, pwd: "new", avatar:null,type: "user"},
+    {userName: "Juliet", userId:9, pwd: "what",avatar:null, type: "user"},
+    {userName: "Admin", userId:10, pwd: "what",avatar:null, type: "admin"},
+    ],
+    projects: 
+        [{id: 0, title: "Project1", avatar: require('./tech.jpg'), status: "in progress", creator: "John Wick"}, 
+        {id: 1, title: "Project2", avatar: null, status: "in progress", creator:"Jane Doe"},
+        {id: 2, title: "Project3", avatar: null, status: "complete", creator: "Mike"},
+        {id: 3, title: "Project4", avatar: require('./southeast.jpg'), status: "in progress", creator: "Alex"},
+        {id: 4, title: "Project5", avatar: null, status: "in progress", creator: "Lucy"},
+        {id: 5, title: "Project6", avatar: null, status: "complete", creator: "Wendy"}]
+}
+
+//remove the user with id
+function removeUser(id){
+    for (let i = 0; i < websiteData.users.length; i++){
+        if (websiteData.users[i].userId === id){
+            websiteData.users.splice(i,1);
+            return;            
+        }
+    }
+}
+
+function removeProj(id){
+    for (let i = 0; i < websiteData.projects.length; i++){
+        if (websiteData.projects[i].id === id){
+            websiteData.projects.splice(i,1);
+            return;            
+        }
+    }
+}
+
+function updateUserInfo(id, name, pwd){
+    for (let i = 0; i < websiteData.users.length; i++){
+        if (websiteData.users[i].userId === id){
+            websiteData.users[i].userName = name? name:websiteData.users[i].userName;
+            websiteData.users[i].pwd = pwd? pwd:websiteData.users[i].pwd;
+            return;
+        }
+    }
+}
+
+function updateProjInfo(id, title, status){
+    for (let i = 0; i < websiteData.users.length; i++){
+        if (websiteData.projects[i].id === id){
+            websiteData.projects[i].title = title? title:websiteData.projects[i].title;
+            websiteData.projects[i].status = status? status:websiteData.projects[i].status;
+            return;
+        }
+    }
+}
 //check if a file uploaded is valid
 //Change it in phase 2
 function beforeUpload(file) {
@@ -43,8 +107,7 @@ function beforeUpload(file) {
 
     //for phase 1 it always returns false to prevent default behaviour try to make a sever call
     return false;
-  }
-   
+  }   
 
 class UploadProfilePicture extends React.Component{
     state = {loading: false}
@@ -83,7 +146,7 @@ class SettingProfileForm extends React.Component{
     state = {       
         firstName: null,
         lastName: null,
-        school: null, 
+        orgnazation: null, 
         email: null,
         phone: null,
         bio: null,
@@ -109,7 +172,7 @@ class SettingProfileForm extends React.Component{
         userdata.firstName = this.state.firstName? this.state.firstName: userdata.firstName;
         userdata.lastName = this.state.lastName? this.state.lastName: userdata.lastName;
         userdata.email = this.state.email? this.state.email: userdata.email;
-        userdata.school = this.state.school? this.state.school: userdata.school;     
+        userdata.orgnazation = this.state.orgnazation? this.state.orgnazation: userdata.orgnazation;     
      };
 
     render(){
@@ -131,13 +194,6 @@ class SettingProfileForm extends React.Component{
             },
           }; 
           
-          const uploadButton = (
-            <div style = {{left: "50px"}}>
-              <Icon type={this.state.loading ? 'loading' : 'plus'} />
-              <div className="ant-upload-text">Upload</div>
-            </div>
-          );
-        
         return(           
                    
         <Form onSubmit={this.handleSubmit}>
@@ -147,8 +203,8 @@ class SettingProfileForm extends React.Component{
             <Form.Item label="Last Name" {...formItemLayout}>
             <Input name = "lastName" placeholder="input your first name" onChange={this.handleInputChange}/>
             </Form.Item>
-            <Form.Item label="School" {...formItemLayout}>
-            <Input name = "school" placeholder="input your school" onChange={this.handleInputChange}/>
+            <Form.Item label="Orgnazation" {...formItemLayout}>
+            <Input name = "orgnazation" placeholder="input your orgnazation" onChange={this.handleInputChange}/>
             </Form.Item>
             <Form.Item label="Email" {...formItemLayout}>
             <Input name = "email" placeholder="input your email address" onChange={this.handleInputChange}/>
@@ -325,7 +381,7 @@ class ProfileDisplay extends React.Component{
                 <Descriptions.Item id ='profile-username'label="User Name">{userdata.userName}</Descriptions.Item>
                 <Descriptions.Item id ='profile-firstname' label="First Name">{userdata.firstName}</Descriptions.Item>
                 <Descriptions.Item id ='profile-firstname' label="Last Name">{userdata.lastName}</Descriptions.Item>
-                <Descriptions.Item label="School">{userdata.school}</Descriptions.Item>
+                <Descriptions.Item label="Orgnazation">{userdata.orgnazation}</Descriptions.Item>
                 <Descriptions.Item label="Email">{userdata.email}</Descriptions.Item>
                 <Descriptions.Item label="Phone Number:">{userdata.phone}</Descriptions.Item>
                 <Descriptions.Item id ='profilebio' label="Bio">{userdata.bio}</Descriptions.Item>
@@ -365,7 +421,18 @@ const tabList = [
     {
         key: 'settings',
         tab: 'Settings',
+    },
+
+    {
+        key: 'userlist',
+        tab: 'Mange Users',
+    },
+
+    {
+        key: 'projlist',
+        tab: 'Mange Projects',
     }
+
   ];
 
 class ProjectList extends React.Component{
@@ -434,7 +501,330 @@ class ProjectList extends React.Component{
       }
   }
 
-class AdminView extends React.Component{
+class ModalUserUpdater extends React.Component{
+    render(){
+        const { visible, onCancel, onUpdate, form } = this.props;
+        const { getFieldDecorator } = form;
+        return(
+            <Modal 
+                visible={visible}
+                title="Update User information"
+                okText="Update"
+                onCancel={onCancel}
+                onOk={onUpdate}>
+                <Form layout="vertical">
+                    <Form.Item label="New username" hasFeedback>
+                        {getFieldDecorator('username', {
+                        rules: [
+                        {
+                            required: false
+                        },
+                        
+                        ],
+                        })(<Input/>)}
+                
+                    </Form.Item>
+                        <Form.Item label="password" hasFeedback>
+                            {getFieldDecorator('password', {
+                            rules: [
+                            {
+                                required: false
+                               
+                            }
+                                
+                            ],
+                            })(<Input.Password />)}
+                        </Form.Item>
+                    </Form>         
+            </Modal>
+        )
+    }    
+
+}
+const UpdateUserForm = Form.create()(ModalUserUpdater)
+class UserManager extends React.Component{
+    state = {
+        visible: false,        
+    }
+
+    saveFormRef = formRef => {
+        this.formRef = formRef;
+    };
+
+    show = () => {        
+        this.setState({ visible: true })
+    }
+
+    cancel = () => {
+        this.setState({ visible: false});
+      };
+
+    onDelete(id){
+        log('delete user with id ', id);
+        removeUser(id);
+        let userlist = websiteData.users
+        this.setState({userlist});        
+    }
+
+    onEdit = (id) => {        
+        this.show();
+        this.setState({editing:id})
+       
+    }
+
+    onSubmit = () => {        
+        const { form } = this.formRef.props;
+        form.validateFields((err, values) => {
+            if (err) {
+            return;
+            }
+            
+            log('Change user info with id:', this.state.editing)
+            let id = this.state.editing
+            
+            updateUserInfo(id,values.username,values.password);
+                      
+            form.resetFields();
+            this.setState({ visible: false});});
+    }
+
+    render()   
+    {
+        this.onDelete = this.onDelete.bind(this);
+        this.onEdit = this.onEdit.bind(this);          
+        const columns = [
+            {
+                title: "Avatar",
+                dataIndex: 'avatar',
+                key: 'avatar',
+                render: picture => <Avatar src={picture}/>
+            },
+
+            {
+                title: "User Name",
+                dataIndex: 'userName',
+                key: 'userName',                
+                render: text => <a>{text}</a>
+            },
+
+            {
+                title: "User Id",
+                dataIndex: 'userId',
+                key: 'userId'                
+            },
+            
+             {
+                title: "User Type",
+                dataIndex: 'type',
+                key: 'userType',
+                render: type => 
+                    <Tag color={type === 'user'? 'blue': 'gold'} key={type}>
+                        {type.toUpperCase()}
+                    </Tag>
+                   
+            },
+
+            {
+                title: "Password",
+                dataIndex: 'pwd',
+                key: 'pwd',               
+            },
+
+           {
+                title: 'Action',
+                key: 'action',
+                render: (text,record, index) =>                    
+                 (
+                    <span>
+                        <Icon type = 'form' onClick={this.onEdit.bind(this,record.userId)}/>                         
+                        <Divider type="vertical"/>                        
+                        <Popconfirm title="Sure to delete the user?" onConfirm={this.onDelete.bind(this,record.userId)}>
+                            <Icon type = 'user-delete'/>
+                        </Popconfirm>
+                  </span>)                
+                
+              },
+
+        ];
+        return(
+            <div>
+                <Table columns={columns} dataSource={websiteData.users}/>
+                <UpdateUserForm
+                    wrappedComponentRef={this.saveFormRef}
+                    visible={this.state.visible}
+                    onCancel={this.cancel}
+                    onUpdate={this.onSubmit}
+                    handleChange = {this.handleInputChange}
+                />              
+            </div>
+        )
+    }
+}
+
+class ModalProjUpdater extends React.Component{
+    render(){
+        const { visible, onCancel, onUpdate, form } = this.props;
+        const { getFieldDecorator } = form;
+        return(
+            <Modal 
+                visible={visible}
+                title="Update Project information"
+                okText="Update"
+                onCancel={onCancel}
+                onOk={onUpdate}>
+                <Form layout="vertical">
+                    <Form.Item label="New project title" hasFeedback>
+                        {getFieldDecorator('title', {
+                        rules: [
+                        {
+                            required: false
+                        },
+                        
+                        ],
+                        })(<Input/>)}
+                
+                    </Form.Item>
+                        <Form.Item label="Change status" hasFeedback>
+                            {getFieldDecorator('status', {
+                            rules: [
+                            {
+                                required: false
+                               
+                            }
+                            ],
+                            })(<Select>
+                                <Option value="in progress">in progress</Option>
+                                <Option value="compelete">compelete</Option>                                
+                              </Select>)}
+                        </Form.Item>
+                    </Form>         
+            </Modal>
+        )
+    }    
+
+}
+
+const UpdateProjForm = Form.create()(ModalProjUpdater)
+
+class ProjManager extends React.Component{
+    state = {
+        visible: false,        
+    }
+
+    saveFormRef = formRef => {
+        this.formRef = formRef;
+    };
+
+    show = () => {        
+        this.setState({ visible: true })
+    }
+
+    cancel = () => {
+        this.setState({ visible: false});
+      };
+
+    onDelete(id){
+        log('delete project with id ', id);
+        removeProj(id);
+        let projlist = websiteData.projects
+        this.setState({projlist});        
+    }
+
+    onEdit = (id) => {        
+        this.show();
+        this.setState({editing:id})
+       
+    }
+
+    onSubmit = () => {        
+        const { form } = this.formRef.props;
+        form.validateFields((err, values) => {
+            if (err) {
+            return;
+            }
+            
+            log('Change project info with id:', this.state.editing)
+            let id = this.state.editing
+            
+            updateProjInfo(id,values.title,values.status);
+                      
+            form.resetFields();
+            this.setState({ visible: false});});
+    }
+
+    render()   
+    {
+        this.onDelete = this.onDelete.bind(this);
+        this.onEdit = this.onEdit.bind(this);          
+        const columns = [
+            {
+                title: "Avatar",
+                dataIndex: 'avatar',
+                key: 'avatar',
+                render: picture => <Avatar src={picture}/>
+            },
+
+            {
+                title: "Project Title",
+                dataIndex: 'title',
+                key: 'title',                
+                render: text => <a>{text}</a>
+            },
+
+            {
+                title: "Project Id",
+                dataIndex: 'id',
+                key: 'id'                
+            },
+            
+             {
+                title: "Project Status",
+                dataIndex: 'status',
+                key: 'status',
+                render: status => 
+                    <Tag color={status === 'in progress'? 'green': 'blue'} key={status}>
+                        {status.toUpperCase()}
+                    </Tag>
+                   
+            },
+
+            {
+                title: "Creator",
+                dataIndex: 'creator',
+                key: 'creator', 
+                render: text => <a>{text}</a>              
+            },
+
+           {
+                title: 'Action',
+                key: 'action',
+                render: (text,record, index) =>                    
+                 (
+                    <span>
+                        <Icon type = 'form' onClick={this.onEdit.bind(this,record.id)}/>                         
+                        <Divider type="vertical"/>                        
+                        <Popconfirm title="Sure to delete the Project?" onConfirm={this.onDelete.bind(this,record.id)}>
+                            <Icon type = 'delete'/>
+                        </Popconfirm>
+                  </span>)                
+                
+              },
+
+        ];
+        return(
+            <div>
+                <Table columns={columns} dataSource={websiteData.projects}/>
+                <UpdateProjForm
+                    wrappedComponentRef={this.saveFormRef}
+                    visible={this.state.visible}
+                    onCancel={this.cancel}
+                    onUpdate={this.onSubmit}                    
+                />              
+            </div>
+        )
+    }
+}
+class UserView extends React.Component{
     constructor(props) {
         super(props);
         this.rerenderParentCallback = this.rerenderParentCallback.bind(this);
@@ -460,7 +850,10 @@ class AdminView extends React.Component{
         const contentListNoTitle = {
             project: <ProjectList key="projlst" rerenderParentCallback={this.rerenderParentCallback}/>,      
             profile: userinfo,
-            settings: settingContent
+            settings: settingContent,
+            userlist:<UserManager/>,
+            projlist: <ProjManager/>            
+
           };
         return(
             <Layout>                
@@ -468,8 +861,10 @@ class AdminView extends React.Component{
                     <Card style = {{margin: '50px', float: "left"}} bordered={false}>                       
                         <Avatar size ={400} shape="square" src={userdata.profileImage}/>
                         <br/>                       
-                        <p><font size="6">{userdata.userName}<Icon type="edit" key="edit" /></font></p>
-                        <p><font size="4">{userdata.bio}<Icon type="edit" key="edit" /></font></p>
+                        <p><font size="6">{userdata.userName}</font></p>
+                        <p><font size="4">{userdata.bio}</font></p>
+                        <br/>
+                        <Tag color="gold">Admin</Tag>
                     </Card>
 
                     <Card
@@ -492,6 +887,4 @@ class AdminView extends React.Component{
     }
 
 }
-
-
-export default AdminView;
+export default UserView;
